bb6886b4c02fc0b1e283df0adbbe2249fe4ac7a1
= <dhruv.mangal@hiteshi.com>
2021-07-30 09:47:14 +0530

    build 001

---
+++ precache-manifest.400c21c3df14a7addffbfb3dadbcd24f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475a151585e8afd896e8",
+    "url": "/css/about.6097cfee.css"
+  },
+  {
+    "revision": "e98a6ff2d8b6fc423468",
+    "url": "/css/app.495e659e.css"
+  },
+  {
+    "revision": "8e9f75d57c22a7e8c69d",
+    "url": "/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "4dd0bf7fb32ef5dfa108fce295186695",
+    "url": "/index.html"
+  },
+  {
+    "revision": "475a151585e8afd896e8",
+    "url": "/js/about.3b85a2da.js"
+  },
+  {
+    "revision": "e98a6ff2d8b6fc423468",
+    "url": "/js/app.c1c0b0ec.js"
+  },
+  {
+    "revision": "8e9f75d57c22a7e8c69d",
+    "url": "/js/chunk-vendors.46618149.js"
+  },
+  {
+    "revision": "ad1c7122216ff6e919bd68f90e80d94d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
